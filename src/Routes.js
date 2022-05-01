789899201a60406fec7116465021807f3c346daa
Aman Singh <imaman07singh@gmail.com>
2022-05-01 15:10:09 +0530

    admin panel folder structure

---
+++ src/Routes.js
@@ -0,0 +1,47 @@
+import {
+  useRoutes,
+  Navigate
+} from 'react-router-dom';
+
+// Layouts
+import DashboardLayout from './layouts/dashboardLayout/DashboardLayout';
+import AuthLayout from './layouts/authLayout/AuthLayout';
+
+// Pages
+import Login from './pages/Login';
+import ResetPassword from './pages/ResetPassword';
+import Conversations from './pages/Conversations';
+import Members from './pages/Members';
+import Speakers from './pages/Speakers';
+import Publications from './pages/Publications';
+import UserMessages from './pages/UserMessages'
+import Page404 from './pages/Page404'
+
+
+const Routes = () => {
+  return useRoutes([
+    {
+      path: '/',
+      element: <AuthLayout />,
+      children: [
+        { path: '/', element: <Login /> },
+        { path: 'reset', element: <ResetPassword /> },
+        { path: '404', element: <Page404 /> }
+      ]
+    },
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { path: 'conversations', element: <Conversations /> },
+        { path: 'members', element: <Members /> },
+        { path: 'speakers', element: <Speakers /> },
+        { path: 'publications', element: <Publications /> },
+        { path: 'messages', element: <UserMessages /> },
+      ]
+    },
+    { path: '*', element: <Navigate to='/404' replace /> }
+  ]);
+}
+
+export default Routes
---
+++ src/pages/Page404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image404 from '../assets/images/illustration_404.svg';
+
+const Page404 = () => {
+  return (
+    <div>
+      <img src={Image404} alt="page not found" />
+    </div>
+  )
+}
+
+export default Page404
